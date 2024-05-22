ed4e1d5f6137681e452acc843dfe037f4ad2d343
gabrielbwitte <wittegabrielb@gmail.com>
2024-05-21 20:42:49 -0400

    Formulario da aula 12

---
+++ src/investmenstGoals.js
@@ -0,0 +1,44 @@
+function converToMontlyReturRate(yearlyReturnRate){
+    return yearlyReturnRate ** (1/12);
+}
+
+function generateReturnsArray(
+    staringAmount = 0, 
+    timeHorizon = 0, 
+    timePeriod = 'monthly', 
+    mothlyContribution = 0, 
+    returnRate = 0,
+     returnTimeFrame = 'monthly'
+    ){
+        if(!timeHorizon || !staringAmount) {
+            throw new Error('Investimento inicial e prazo devem ser preenchidos com valores positivos');
+        }
+
+        const finalReturnRate = returnTimeFrame === 'monthly' ? 1 + returnRate / 100: converToMontlyReturRate(1 + returnRate / 100);
+
+        const fianlTimeHorizon = timePeriod === 'monthly' ? timeHorizon: timeHorizon * 12;
+
+        const referenceInvestmentObject = {
+            investedAmount: startingAmount,
+            interesReturns: 0,
+            totalInteresReturns: 0,
+            month: 0,
+            totalAmount: staringAmount,
+        };
+
+        const returnsArray = [referenceInvestmentObject];
+        for(let timeReference = 1; timeReference <= fianlTimeHorizon; timeReference++) {
+            const totalAmount = (returnsArray[timeReference - 1].totalAmount * finalReturnRate) + mothlyContribution;
+            const interesReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+            const investedAmount = startingAmount + mothlyContribution * timeReference;
+            const totalInteresReturns = totalAmount - investedAmount;
+
+            returnsArray.push({
+            investedAmount,
+            interesReturns,
+            totalInteresReturns,
+            month: timeReference,
+            totalAmount,
+            })
+        }
+}
