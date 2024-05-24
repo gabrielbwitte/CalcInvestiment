07cffd7a1e4c980c2efac6b91f2aabb74e815279
gabrielbwitte <wittegabrielb@gmail.com>
2024-05-24 19:58:47 -0400

    dia 24

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import { generateReturnsArray } from './src/investmenstGoals';
+import { Chart } from 'chart.js/auto';
+
+const finalMoneyChart = document.getElementById('final-money-distribuion');
+const progressionChart = document.getElementById('progression');
 
 const form = document.getElementById('investment-form');
 const clearFormButton = document.getElementById('clear-form');
@@ -22,7 +26,30 @@ function renderProgression(evt){
     const returnArray = generateReturnsArray(staringAmount, timeAmount, timeAmountPeriod, additionalContribution, returnRate, returnRatePeriod)
 
 
-    console.log(returnArray);
+    //console.log(returnArray);
+
+    new Chart(finalMoneyChart, {
+        
+            type: 'doughnut',
+            data: {
+                labels: [
+                  'Red',
+                  'Blue',
+                  'Yellow'
+                ],
+                datasets: [{
+                  label: 'My First Dataset',
+                  data: [300, 50, 100],
+                  backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                  ],
+                  hoverOffset: 4
+                }],
+              },
+          
+    })
 } 
 
 function clearForm(){
